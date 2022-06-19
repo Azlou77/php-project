a4b69d5648dd351a02ad335a9fc290f321d2b555
Azlou77 <nguyenlouis172@gmail.com>
2022-06-19 23:08:22 +0200

    challenge2

--- exercices/exercises-challenge/challenge2.php
+++ exercices/exercises-challenge/challenge2.php
@@ -5,3 +5,21 @@
     -bonus check  if the guessed is out of range
 
 */
+
+$min = 1; 
+$max = 50;
+$guess = 16; //Change this value to test!
+$num = 16; // Change this value to test!
+
+if ($guess == $num) {
+    echo "You guessed correctly!";
+} elseif ($guess > $num) {
+    echo "Your guess was too high!";
+} elseif ($guess < $num) {
+    echo "Your guess was too low!";
+} else {
+    echo "Your guess was out of range!";
+}
+// If you really want to go nuts, try this:
+//$num = rand($min, $max);
+?>
