30d0871e35310a9ab977157b9b6495de481d4fd9
Azlou77 <nguyenlouis172@gmail.com>
2022-06-24 11:17:17 +0200

    third section

---
+++ cours/course-section3.php
@@ -0,0 +1,67 @@
+<?php
+/*In this course, I learn   arithmetic operators,
+ 
+
+*/
+/*After addition, subtraction, multiplication, division, we see 
+  parentheses, eponentiation.
+*/
+echo 5 * 6 +3- 1;
+//results:32
+echo 5 * (6 +3)- 1;
+/*results: 44 because here,
+  we calcul the addition in parentheses first,
+    then we calculate the multiplication.
+Modulus operators:
+$a=2;
+$b=3;
+$b % $a //output 1
+$c= 15;
+$d = 5;
+$c % $d //output 3
+Shortland increment, decrement 
+$a = $a +1
+$a++
+$b = $b- 1
+$b--
+
+The functions: reusable snippets of code that can be called 
+multiples times
+The built-in-functions = functions natives 
+view in php manual
+
+optionnal or named arguments
+function multiplier ($z, $b)
+{
+  return $a * $multipliee
+}
+echo multiplier (4,4);
+
+Anonymous functions or callbacks functions
+$names = array
+(
+  "Joe",
+  "Louis,
+  "Brenda
+);
+usort allow to comapre greater than, less than, equal.
+
+Class is a way of grouping variables and functions as a single unit
+The classes have objects: instances of a class
+Variables in classes called "properties"
+Function in classes called "methods"
+
+Example:
+//create 1st object
+$louis = new Person ("Louis");
+
+//create 2nd object
+$brenda = new Person ("Brenda");
+
+//Get name
+echo $louis->get_name; / echo $brenda->get_name;
+
+//Set name
+echo $louis->set_name(32); / echo $brenda->set_name(46);
+*/
+ 
